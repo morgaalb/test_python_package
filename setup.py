1f793545069e88626358a4309d6d253c64edaca4
Albert Morgan <morgaalb@oregonstate.edu>
2018-03-17 19:23:32 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="test_python_package",
+    version="0.1",
+    packages=find_packages()    
+)
---
+++ test_python_package/__init__.py
@@ -0,0 +1,3 @@
+def foo():
+    print "IT'S ALIVE!"
+    
